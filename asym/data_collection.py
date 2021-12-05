3139eb673f603d1e3cc9c622eaebb202f63955b5
daeseoklee <dsleemaths@gmail.com>
2021-12-05 03:22:12 +0000

    1. simplified hash checks  2. DC.groups_as_data() and DC.masks_as_data()

--- asym/data_collection.py
+++ asym/data_collection.py
@@ -22,7 +22,7 @@ class IncompatibleDataCollectionError(Exception):
         return self.message
 
 class DataCollection:
-    def __init__(self, shape_annot:Union[ShapeSignatureData, Dict[str, Union[str, Any]]], data_list:List[TensorData]=None, data_groups:List[TensorData]=None, length_info:Dict[str, List[List[int]]]=None, data_partition:List[Tuple[int, int]]=None, check_hash=True, _hash=None, padding:Union[PadderData, Padder, Dict[str, Any]]=None, validate=False):
+    def __init__(self, shape_annot:Union[ShapeSignatureData, Dict[str, Union[str, Any]]], data_list:List[TensorData]=None, data_groups:List[TensorData]=None, length_info:Dict[str, List[List[int]]]=None, data_partition:List[Tuple[int, int]]=None, padding:Union[PadderData, Padder, Dict[str, Any]]=None, validate=False):
         """ 
         There are two ways of initializing DataCollection:
             1. providing a data_list 
@@ -31,13 +31,20 @@ class DataCollection:
                 -length_info
                 -data_partition (optional)
         It is always either in "grouped" or "ungrouped" form. Being grouped means the data list is partitioned into several parts and each part form a singe TensorData object after paddings
-        
-        The "compatibility" is tracked along method calls using the internal _hash value. Each call of .ungroup() or .group() change the _hash value. For example, one cannot dc1[key] = dc2 if dc1._hash != dc2._hash   
         """
         if type(shape_annot) == ShapeSignatureData:
             self.shapesig_data = shape_annot
         else:
             self.shapesig_data = ShapeSignatureData.parse(shape_annot)
+
+        if padding is not None:
+            if type(padding) == PadderData:
+                self.preset_padder_data = padding
+            else:
+                self.preset_padder_data = PadderData(padding, template=self.shapesig_data.get_template())
+        else:
+            self.preset_padder_data = None
+            
         if data_list is not None:
             if data_groups is not None or length_info is not None or data_partition is not None:
                 raise Exception('either data_list or all of [data_groups, length_info, data_partition] should be provided')
@@ -49,34 +56,36 @@ class DataCollection:
         elif data_groups is not None:
             if data_list is not None or length_info is None:
                 raise Exception('either data_list or all of [data_groups, length_info] should be provided')
+            if type(data_groups) != list:
+                raise Exception('data_groups should be a list')
+            if len(data_groups) == 0:
+                raise Exception('data_groups should be a non-empty list')
+            self.data_list = None
+            if type(data_groups[0]) == list:
+                pass
+            else:
+                self.data_groups = [group if type(group) == TensorData else TensorData(group) for group in data_groups]
             if data_partition is None:
                 data_partition = DataCollection.get_default_data_partition(data_groups, self.shapesig_data) 
-            self.data_list = None
-            self.data_groups = [group if type(group) == TensorData else TensorData(group) for group in data_groups]
             self.length_info = length_info
             self.data_partition = data_partition
             self.is_grouped = True
         else:
             raise Exception('either data_list or all of [data_groups, length_info] should be provided')
-        if padding is not None:
-            if type(padding) == PadderData:
-                self.preset_padder_data = padding
-            else:
-                self.preset_padder_data = PadderData(padding, template=self.shapesig_data.get_template())
-        else:
-            self.preset_padder_data = None
+
         if validate:
             self.validate()
-        self.check_hash = check_hash
-        self.update_hash_(given_hash=_hash)
 
-    def update_hash_(self, given_hash=None):
-        if not self.check_hash:
-            return
-        if given_hash is None:
-            self._hash = time()
-        else:
-            self._hash = given_hash
+            
+    def get_group_hash(self):
+        if self.is_grouped:
+            assert self.data_partition is not None 
+            assert type(self.data_partition) == list
+            assert type(self.data_partition[0]) == list
+            assert type(self.data_partition[0][0]) == int 
+            return hash(tuple([hash(tuple(part)) for part in self.data_partition]))
+        assert self.data_list is not None
+        return len(self.data_list)
 
     def __len__(self):
         if self.is_grouped:
@@ -128,26 +137,27 @@ class DataCollection:
         return {key: val[i] for key, val in self.length_info.items()}
         
     
+    def get_data_group(self, data_sublist:List[TensorData], shapesig_data:ShapeSignatureData, padder_data:PadderData) -> TensorData:
+        first_data = data_sublist[0]
+        if not first_data.is_leaf:
+            d = {key: self.get_data_group([data[key] for data in data_sublist], shapesig_data[key], padder_data[key]) for key in first_data.keys()}
+            return TensorData.from_data_dict(d)
+
+        assert padder_data.is_leaf
+        padder = padder_data.value
+        
+        assert hasattr(padder, 'pad')
+        assert shapesig_data.is_leaf
+        shapesig = shapesig_data.value
+        return TensorData(padder.merge_tensors([data.value for data in data_sublist], shapesig))
+    
+    
     def get_data_groups(self, partition:List[List[int]], padder_data:PadderData) -> List[TensorData]:
         if self.is_grouped:
             return self.data_groups
         assert self.data_list is not None
         
-        def _get_data_group(data_sublist:List[TensorData], shapesig_data:ShapeSignatureData, padder_data:PadderData) -> TensorData:
-            first_data = data_sublist[0]
-            if not first_data.is_leaf:
-                d = {key: _get_data_group([data[key] for data in data_sublist], shapesig_data[key], padder_data[key]) for key in first_data.keys()}
-                return TensorData.from_data_dict(d)
-
-            assert padder_data.is_leaf
-            padder = padder_data.value
-            
-            assert hasattr(padder, 'pad')
-            assert shapesig_data.is_leaf
-            shapesig = shapesig_data.value
-            return TensorData(padder.merge_tensors([data.value for data in data_sublist], shapesig))
-        
-        return [_get_data_group([self.data_list[i] for i in part], self.shapesig_data, padder_data) for part in partition]
+        return [self.get_data_group([self.data_list[i] for i in part], self.shapesig_data, padder_data) for part in partition]
             
     
     def get_data_list(self) -> List[TensorData]:
@@ -204,12 +214,11 @@ class DataCollection:
         self.length_info = self.get_length_info(data_partition)
         self.data_groups = self.get_data_groups(data_partition, padder_data)
         if forget_order:
-            self.data_partition = None
+            self.data_partition = self.get_default_data_partition(self.data_groups, self.shapesig_data)
         else:
             self.data_partition = data_partition
         self.data_list = None
         self.is_grouped = True
-        self.update_hash_()
         
     def ungroup(self):
         if not self.is_grouped:
@@ -220,56 +229,120 @@ class DataCollection:
         self.length_info = None
         self.data_partition = None
         self.is_grouped = False
-        self.update_hash_()
 
     def regroup(self, grouper:DataListGrouper, padding:Union[Padder, Dict[str, Any]]=None, forget_order=False):
         self.ungroup()
         self.group(grouper, padding=padding, forget_order=forget_order)
+        
+    def _get_new_shapesig(self, shapesig: ShapeSignature, new_bdim_idx:int, new_ldim_label:str):
+        if new_bdim_idx >= 0:
+            if new_bdim_idx > len(shapesig):
+                raise Exception(f'new_bdim_idx {new_bdim_idx} is too big compared to {shapesig}')
+            _new_bdim_idx = 0
+        else:
+            if new_bdim_idx < - len(shapesig) - 1:
+                raise Exception(f'new_bdim_idx {new_bdim_idx} is too small compared to {shapesig}')
+            _new_bdim_idx = new_bdim_idx + len(shapesig) + 1
+        l = []
+        for dim in shapesig.list:
+            if type(dim) == BDim:
+                l.append(LDim(new_ldim_label))
+            elif type(dim) == LDim and dim.label == new_ldim_label:
+                raise Exception(f'The LDim label {dim.label} already exists in {shapesig}')
+            else:
+                l.append(dim)
+        l.insert(_new_bdim_idx, BDim())
+        return ShapeSignature(l)
+        
+    def groups_as_data(self, bdim_idx:int, new_ldim_label:str, padding=None):
+        if not self.is_grouped:
+            raise Exception('"groups_as_data()" can be called only when the DataCollection is grouped')
+        data_list = self.data_groups
+        
+        shapesig_conversion = lambda shapesig: self._get_new_shapesig(shapesig, bdim_idx, new_ldim_label)
+        shapesig_data = ShapeSignatureData.map(shapesig_conversion, self.shapesig_data)
+        
+        if padding is None: #We assume that after groups_as_data, same preset_padder_data works
+            padding = self.preset_padder_data
+        
+        return DataCollection(shapesig_data, data_list=data_list, padding=padding)
+    
+    def masks_as_data(self, bdim_idx:int, new_ldim_label:str, padding=None):
+        if not self.is_grouped:
+            raise Exception('"masks_as_data()" can be called only when the DataCollection is grouped')
+        data_list = [self.get_mask(i, mask_hint='copy') for i in range(len(self.data_groups))]
+
+        shapesig_conversion = lambda shapesig: self._get_new_shapesig(shapesig, bdim_idx, new_ldim_label)
+        shapesig_data = ShapeSignatureData.map(shapesig_conversion, self.shapesig_data)
+        
+        return DataCollection(shapesig_data, data_list=data_list, padding=padding)
 
     def get_grand_batch(self):
         pass
     
-    def get_mask_data(self, i:int, key_conv=None) -> TensorData:
-        group = self.data_groups[i]
+    def get_mask(self, i:int, key_conv=None, mask_hint:Union[str, List[str]]='copy') -> Union[Tensor, Dict[str, Any]]:
         length_info = self.get_group_length_info(i)
-        def _get_mask(t:Tensor, shapesig:ShapeSignature):
+        group = self.data_groups[i]
+        
+        if mask_hint == 'copy':
+            def _get_mask(t:Tensor, shapesig:ShapeSignature):
 
-            bdim_idx = shapesig.bdim_idx 
-            ldim_label_map = {idx: label for idx, label in shapesig.iter_ldim_idx_label()}
-            if len(ldim_label_map) == 0:
-                return torch.ones_like(t, dtype=bool)
-            inst_mask_shape = t.shape[:bdim_idx] + (1,) + t.shape[bdim_idx+1:]
+                bdim_idx = shapesig.bdim_idx 
+                ldim_label_map = {idx: label for idx, label in shapesig.iter_ldim_idx_label()}
+                if len(ldim_label_map) == 0:
+                    return torch.ones_like(t, dtype=bool)
+                inst_mask_shape = t.shape[:bdim_idx] + (1,) + t.shape[bdim_idx+1:]
+                
+                def get_shape_at(dim_idx):
+                    return (1,) * dim_idx + (t.shape[dim_idx],) + (1,) * (len(t.shape) - dim_idx - 1)
+                
+                def _get_mask_at(inst_idx):
+                    masks = [(torch.arange(t.shape[ldim_idx], device=t.device) < length_info[ldim_label][inst_idx]).reshape(get_shape_at(ldim_idx)).broadcast_to(inst_mask_shape) for ldim_idx, ldim_label in ldim_label_map.items()]
+                    return torch.any(torch.stack(masks, dim=0), dim=0)
+                
+                masks = [_get_mask_at(inst_idx) for inst_idx in range(t.shape[bdim_idx])]
+                return torch.cat(masks, dim=bdim_idx) 
             
-            def get_shape_at(dim_idx):
-                return (1,) * dim_idx + (t.shape[dim_idx],) + (1,) * (len(t.shape) - dim_idx - 1)
+            mask_data = TensorData.map2(_get_mask, group, self.shapesig_data)
+            if key_conv is not None:
+                mask_data = mask_data.keys_converted(key_conv=key_conv)
+            return mask_data.value
+        
+        elif type(mask_hint) == list:
+            device = group.get_any_leaf_value().device
+            if len(mask_hint) == 0:
+                raise Exception(f'mask_hint should be a non-empty list of strings')
+            for ldim_label in mask_hint:
+                if not ldim_label in length_info:
+                    raise Exception(f'The string {ldim_label} in the list mask_hint is not a LDim label')
+            N = len(length_info[ldim_label])
+
+            inst_mask_shape = tuple([max(length_info[ldim_label]) for ldim_label in mask_hint])
+            
+            def get_shape_at(ldim_idx):
+                return (1,) * ldim_idx + (inst_mask_shape[ldim_idx],) + (1,) * (len(inst_mask_shape) - ldim_idx - 1)
             
             def _get_mask_at(inst_idx):
-                masks = [(torch.arange(t.shape[ldim_idx], device=t.device) < length_info[ldim_label][inst_idx]).reshape(get_shape_at(ldim_idx)).broadcast_to(inst_mask_shape) for ldim_idx, ldim_label in ldim_label_map.items()]
+                masks = [(torch.arange(inst_mask_shape[ldim_idx], device=device) < length_info[ldim_label][inst_idx]).reshape(get_shape_at(ldim_idx)).broadcast_to(inst_mask_shape) for ldim_idx, ldim_label in enumerate(mask_hint)]
                 return torch.any(torch.stack(masks, dim=0), dim=0)
             
-            masks = [_get_mask_at(inst_idx) for inst_idx in range(t.shape[bdim_idx])]
-            return torch.cat(masks, dim=bdim_idx) 
-        
-        mask_data = TensorData.map2(_get_mask, group, self.shapesig_data)
-        if key_conv is not None:
-            mask_data = mask_data.keys_converted(key_conv=key_conv)
-        return mask_data
+            masks = [_get_mask_at(inst_idx) for inst_idx in range(N)]
+            return torch.stack(masks, dim=0) 
     
     def to_device(self, device):
-        #TODO: Should _hash be updated??
         if self.preset_padder_data is None:
             new_padding = None
         else:
             new_padding = self.preset_padder_data.to_device(device)
         if self.is_grouped:
             new_data_groups = [group.to_device(device) for group in self.data_groups]
-            dc = DataCollection(self.shapesig_data, data_groups=new_data_groups, length_info=self.length_info, data_partition=self.data_partition, check_hash=self.check_hash, _hash=self._hash, padding=new_padding)
+            dc = DataCollection(self.shapesig_data, data_groups=new_data_groups, length_info=self.length_info, data_partition=self.data_partition, padding=new_padding)
         else:
             new_data_list = [data.to_device(device) for data in self.data_list]
-            dc = DataCollection(self.shapesig_data, data_list=new_data_list, check_hash=self.check_hash, _hash=self._hash, padding=new_padding)
+            dc = DataCollection(self.shapesig_data, data_list=new_data_list, padding=new_padding)
         return dc
 
-    def apply(self, module:AnnotatedModule, require_mask=True, input_key_conv=None, output_key_conv=None) -> 'DataCollection':
+    def apply(self, module:AnnotatedModule, mask_hint='copy', input_key_conv=None, output_key_conv=None) -> 'DataCollection':
         """
         """
         if not self.is_grouped:
@@ -279,7 +352,7 @@ class DataCollection:
         
         key_converted_data_groups = [group.keys_converted(input_key_conv) for group in self.data_groups]
         if module.requires_mask():
-            new_data_groups = [TensorData(module(group.value, self.get_mask_data(i, key_conv=input_key_conv).value)) for i, group in enumerate(key_converted_data_groups)]
+            new_data_groups = [TensorData(module(group.value, self.get_mask(i, mask_hint=mask_hint, key_conv=input_key_conv))) for i, group in enumerate(key_converted_data_groups)]
         else:
             new_data_groups = [TensorData(module(group.value)) for group in key_converted_data_groups]
         
@@ -288,7 +361,7 @@ class DataCollection:
                 
         new_length_info = {label: info for label, info in self.length_info.items() if label in output_shapesig_data.ldim_map}
 
-        return DataCollection(output_shapesig_data, data_groups=new_data_groups, length_info=new_length_info, data_partition=self.data_partition, check_hash=self.check_hash, _hash=self._hash)
+        return DataCollection(output_shapesig_data, data_groups=new_data_groups, length_info=new_length_info, data_partition=self.data_partition)
     
     def __getitem__(self, key:str) -> 'DataCollection':
         if self.shapesig_data.is_leaf:
@@ -299,13 +372,13 @@ class DataCollection:
         if self.is_grouped:
             data_groups = [group[key] for group in self.data_groups]
             length_info = {key: val for key, val in self.length_info.items() if key in shapesig_data.ldim_map}
-            return DataCollection(shapesig_data, data_groups=data_groups, length_info=length_info, data_partition=self.data_partition, check_hash=self.check_hash, _hash=self._hash)
+            return DataCollection(shapesig_data, data_groups=data_groups, length_info=length_info, data_partition=self.data_partition)
         else:
             data_list = [data[key] for data in self.data_list]
-            return DataCollection(shapesig_data, data_list=data_list, check_hash=self.check_hash, _hash=self._hash)
+            return DataCollection(shapesig_data, data_list=data_list)
         
     def __setitem__(self, key:str, sub:'DataCollection'):
-        if self.check_hash and self._hash != sub._hash:
+        if self.get_group_hash() != sub.get_group_hash():
             raise IncompatibleDataCollectionError(f'It seems two DataCollections are not compatible, so you cannot __setitem__ one into another.')
         if self.is_grouped:
             for group, sub_group in zip(self.data_groups, sub.data_groups):
